2bb2cbd9e3ae44a11c5479f39e3437672f04a1cb
TheBlackFern <doroginin.dv@phystech.edu>
2023-09-25 21:01:22 +0300

    🐛 fixed: eslint hates non-escaped apostrophes ✨ different phrases for different results

--- src/components/forms/quiz-form-results.tsx
+++ src/components/forms/quiz-form-results.tsx
@@ -14,13 +14,20 @@ type QuizFormResultsProps = {
   children: ReactNode;
 };
 
+function ratingPhrase(rating: number) {
+  if (rating < 0.5) return "Remember, practice makes perfect!";
+  if (rating < 0.8) return "Way to go!";
+  if (rating < 1) return "You&apos;ve done well!";
+  return "Perfect!";
+}
+
 const QuizFormResults = ({ score, total, children }: QuizFormResultsProps) => {
   return (
     <Card className="w-[250px] flex flex-col text-center items-center">
       <CardHeader>
         <CardTitle>Results</CardTitle>
         {/* TODO: analyse the results */}
-        <CardDescription>You've done great!</CardDescription>
+        <CardDescription>{ratingPhrase(score / total)}</CardDescription>
       </CardHeader>
       <CardContent>
         <div className="w-24 h-24 from-primary to-primary-foreground bg-gradient-to-b from-40% rounded-full grid place-content-center">
