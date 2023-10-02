1ac19c52287989e27343565cf464ead5c0a2ad6e
TheBlackFern <97515538+TheBlackFern@users.noreply.github.com>
2023-10-02 21:59:14 +0300

    📱 better responsiveness for test selection

--- src/components/quiz/quiz-link.tsx
+++ src/components/quiz/quiz-link.tsx
@@ -1,8 +1,9 @@
 import { Test } from "@/db/schema";
 import Link from "next/link";
 import React from "react";
-import { Button } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import { ArrowRight, ChevronsRight } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 type QuizLinkProps = {
   test: Test;
@@ -10,23 +11,26 @@ type QuizLinkProps = {
 
 const QuizLink = ({ test }: QuizLinkProps) => {
   return (
-    <Link href={`/test/${test.id}`}>
-      <Button
-        variant={"secondary"}
-        className="group flex flex-row gap-3 h-auto justify-start px-5 py-3"
-      >
-        <p className="text-start">
-          <span className="line-clamp-1 text-lg break-all">{test.topic}</span>
-          <span className="text-muted-foreground line-clamp-1 break-all">
-            {test.description}
-          </span>
-        </p>
-        <ChevronsRight
-          width={24}
-          height={48}
-          className="group-hover:opacity-100 opacity-0 transition-all duration-200"
-        />
-      </Button>
+    <Link
+      href={`/test/${test.id}`}
+      className={cn(
+        buttonVariants({ variant: "secondary" }),
+        "group flex flex-row gap-3 h-auto justify-start px-5 py-3 w-[90vw] sm:w-[70vw] max-w-[600px]"
+      )}
+    >
+      <p className="text-start">
+        <span className="line-clamp-1 text-base sm:text-lg break-all">
+          {test.topic}
+        </span>
+        <span className="text-muted-foreground text-[12px] sm:text-base line-clamp-1 break-all">
+          {test.description}
+        </span>
+      </p>
+      <ChevronsRight
+        width={24}
+        height={48}
+        className="hidden sm:block group-hover:opacity-100 ml-auto opacity-0 transition-all duration-200"
+      />
     </Link>
   );
 };
