b8353e716399d6c20cb92374e69500dd4f86a13b
TheBlackFern <97515538+TheBlackFern@users.noreply.github.com>
2023-10-31 20:20:40 +0300

    🚧 results page, show all results

---
+++ src/app/results/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { fetchResults } from "@/db";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+
+const Page = async () => {
+  const results = await fetchResults();
+  return (
+    <div className="flex flex-col gap-3">
+      {results.map((res) => (
+        <Link
+          href={`/result/${res.id}`}
+          className={cn(buttonVariants({ variant: "secondary" }))}
+        >
+          {res.testtaker} {res.test}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Page;
