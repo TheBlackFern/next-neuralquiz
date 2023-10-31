beb89e9c83b39aa345ad20d3a6e31050ecc0ad81
TheBlackFern <97515538+TheBlackFern@users.noreply.github.com>
2023-10-31 21:12:02 +0300

    💄 heading and grid for the results page

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { fetchResults } from "@/db";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
+
 import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { fetchResults } from "@/db";
 
 const Page = async () => {
   const results = await fetchResults();
   return (
-    <div className="flex flex-col gap-3">
-      {results.map((res) => (
-        <Link
-          href={`/results/${res.id}`}
-          className={cn(buttonVariants({ variant: "secondary" }))}
-        >
-          {res.testtaker} {res.test}
-        </Link>
-      ))}
+    <div className="flex w-full flex-col items-center justify-center gap-5">
+      <h2 className="text-3xl font-medium">Test Results</h2>
+      <div className="grid w-full gap-3 [grid-template-columns:repeat(auto-fill,minmax(150px,1fr))]">
+        {results.map((res) => (
+          <Link
+            href={`/results/${res.id}`}
+            className={cn(
+              buttonVariants({ variant: "secondary" }),
+              "flex h-auto flex-col items-start",
+            )}
+          >
+            <span className="line-clamp-1 break-all">{res.testtaker}</span>
+            <span className="line-clamp-1 break-all">{res.test}</span>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
