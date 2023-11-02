662ab5cfaccb219c3fb318a5d0b75d9a4916d410
TheBlackFern <97515538+TheBlackFern@users.noreply.github.com>
2023-11-02 20:08:05 +0300

    ♻️ quiz submit button component

---
+++ src/components/quiz/quiz-submit-button.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+
+const usernameSchema = z.object({
+  username: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+});
+
+type QuizSubmitButtonProps = {
+  handleSubmit(name: string): void;
+};
+
+export function QuizSubmitButton({ handleSubmit }: QuizSubmitButtonProps) {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const form = useForm<z.infer<typeof usernameSchema>>({
+    resolver: zodResolver(usernameSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof usernameSchema>) {
+    handleSubmit(values.username);
+  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button type="button">Submit</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>One Last Thing</DialogTitle>
+          <DialogDescription>
+            Enter your name, so that the result is stored.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your name" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is the name which will be displayed in the test result.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button type="submit">Submit</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
